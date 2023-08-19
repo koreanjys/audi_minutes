1c8c2e080fb75843e3095d52abd7830aafd563cd
koreanjys <connecttojys@gmail.com>
2023-08-19 14:48:25 +0900

    update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 - 영상들 중에서 거의 유사한 오디오끼리 묶는 프로그램
 
 ## 요청사항
-- 영상 파일들 중에서 오디오가 유사한 영상끼리 짝지어서 각 각 폴더 안에 넣음
+- 영상 파일들 중에서 오디오가 유사한 영상들을 그룹화 시켜서 각 폴더에 넣음
 
 ## 프로젝트 설계
 1. 영상에서 오디오 추출
@@ -13,6 +13,20 @@
 6. 배포하기 위해 ffmpegInstaller 생성(필수 설치)
 7. audi_minutes.exe 실행파일 생성
 
+## 프로그램 진행도
+
+1. 동영상 파일로부터 오디오 추출: `extract_audio_from_video` 함수는 동영상 파일로부터 오디오를 추출하여 임시 WAV 오디오 파일을 생성합니다.
+
+2. 오디오 특징 추출: `extract_features` 함수는 librosa 라이브러리를 사용하여 주어진 오디오 파일에서 MFCC (Mel-frequency cepstral coefficients) 기반의 특징을 추출합니다.
+
+3. 오디오 유사성 계산: `compute_similarity_by_segments` 함수는 입력된 두 오디오 특징 사이의 코사인리를 계산하여 그 오디오들이 얼마나 유사한지를 결정합니다.
+
+4. 비슷한 오디오의 동영상 그룹화 `group_similar_videos` 함수는 유사한 오디오를 가진 동영상 파일을 그룹화하는 데 사용되며, 각 동영상은 하나 이상의 그룹에 속할 수 있습니다.룹화는 `is_video_similar` 함수를 사용하여 이미 그룹화된 오디오와 비교하여진행됩니다.
+
+5. 가장 외부의 `main` 함수: 모든 동영상으로부터 오디오 파일을 추출하고, 오디오 특징을 추출한 다음, 그룹화된 비슷한 오디오영상을 출력합니다.
+
+6. 그룹화된 동영상 파일 이동: `move_videos_to_folders` 함수는 생성된 그룹별로 개별 폴더에영상 파일들을 복사하여 그룹화 결과를 계층적으로 저장합니다.
+
 ## 프로그램 사용방법
 - `audi_minutes.exe`와 같은 경로에 `videos` 폴더가 있어야 한다. (폴더 안에 영상 파일들이 들어가 있으면 된다.)
 - `*.mp4` 파일이어야 한다.
@@ -21,7 +35,7 @@
 
 ## 프로그램 다운로드
 - [ffmpegInstaller](https://drive.google.com/file/d/1iUPL9IkJgkhaJiuBPyb9EuFkELSMjkbW/view?usp=drive_link)
-- [audi_minutes.exe](https://drive.google.com/file/d/1acD-vNzYKNf8Hg4ceP5QfW6cps4vT8id/view?usp=drive_link)
+- [audi_minutes.exe](https://drive.google.com/file/d/1sYqeRo6jBLoEif2Ub_55J5pOdU-nEr5n/view?usp=drive_link)
 
 
 
--- audi_minutes.py
+++ audi_minutes.py
@@ -11,102 +11,110 @@ from moviepy.editor import VideoFileClip
 ffmpeg_exec_path = os.path.join(os.path.dirname(sys.executable), "ffmpeg", "bin")
 os.environ['PATH'] = ffmpeg_exec_path + os.pathsep + os.environ['PATH']
 
-
 def extract_audio_from_video(video_file, output_audio_file):
     video = VideoFileClip(video_file)
     audio = video.audio
     audio.write_audiofile(output_audio_file)
 
-
-def segment_audio(y, sr, segment_duration_sec):
-    n_samples_per_segment = int(sr * segment_duration_sec)
-    n_segments = len(y) // n_samples_per_segment
-
-    segments = []
-    for i in range(n_segments):
-        start_sample = i * n_samples_per_segment
-        end_sample = start_sample + n_samples_per_segment
-        segment = y[start_sample:end_sample]
-        segments.append(segment)
-
-    return segments
-
-
-def extract_features(audio_file, segment_duration_sec=3):
+def extract_features(audio_file, segment_duration_sec=7):
     try:
         y, sr = librosa.load(audio_file)
     except Exception as exc:
         print(f"Error occurred while loading audio file: {exc}")
         return np.zeros((128, segment_duration_sec * sr // 512 + 1))
 
-    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20)
-    return mfcc
+    hop_length = segment_duration_sec * sr // (128 - 1)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20, hop_length=hop_length)
+    return mfcc, sr, hop_length
 
-
-def compute_similarity_by_segments(feature1, feature2):
+def compute_similarity_by_segments(feature1, feature2, sr, hop_length, duration_sec=7):
     if feature1 is None or feature2 is None:
-        return 0
+        return False
 
     min_rows = min(feature1.shape[0], feature2.shape[0])
-    feature1 = feature1[:min_rows]
-    feature2 = feature2[:min_rows]
+    min_cols = min(feature1.shape[1], feature2.shape[1])  # 범위를 벗어나지 않도록 열의 최소 크기를 가져옵니다.
+    feature1 = feature1[:min_rows, :min_cols]
+    feature2 = feature2[:min_rows, :min_cols]
 
-    min_cols = min(feature1.shape[1], feature2.shape[1])
-    score = np.mean([cosine(feature1[:, i], feature2[:, i]) for i in range(min_cols)])
-    return score
+    threshold = 0.4
 
+    required_duration = duration_sec
+    
+    duration_count = 0
+    for i in range(min_cols):  # 최소 열 크기를 사용하여 루프를 실행합니다.
+        try:
+            similarity = cosine(feature1[:, i], feature2[:, i])
+        except ValueError:
+            similarity = 0
+
+        if similarity < threshold:
+            time_diff = librosa.frames_to_time([1], sr=sr, hop_length=hop_length)[0]
+            duration_count += time_diff
+            if duration_count >= required_duration:
+                return True
+        else:
+            duration_count = 0
+    return False
+
+
+def is_video_similar(entry, video_group, extracted_features, sr, hop_length):
+    for video_index in video_group:
+        if compute_similarity_by_segments(
+            extracted_features[entry][0], extracted_features[video_index][0], sr, hop_length
+        ):
+            return True
+    return False
+
+def group_similar_videos(extracted_features, video_files):
+    groups = []
+
+    for i in range(len(extracted_features)):
+        added_to_group = False
+        for group in groups:
+            if is_video_similar(i, group, extracted_features, extracted_features[i][1], extracted_features[i][2]):
+                group.append(i)
+                added_to_group = True
+                break
+
+        if not added_to_group:
+            groups.append([i])
+
+    grouped_videos = []
+    for group in groups:
+        grouped_videos.append([video_files[index] for index in group])
+
+    return grouped_videos
 
 def main(video_files):
     extracted_features = []
+
     for video in video_files:
         output_audio_file = 'temp_audio.wav'
         extract_audio_from_video(video, output_audio_file)
 
-        features = extract_features(output_audio_file)
-        extracted_features.append(features)
+        features, sr, hop_length = extract_features(output_audio_file)
+        extracted_features.append((features, sr, hop_length))
 
-    threshold = 0.17
-    same_audio_videos_dict = {}
+    groups = group_similar_videos(extracted_features, video_files)
 
-    for i in range(len(extracted_features)):
-        for j in range(i + 1, len(extracted_features)):
-            similarity = compute_similarity_by_segments(extracted_features[i], extracted_features[j])
+    print("Grouped similar audio videos:", groups)
+    return groups
 
-            if similarity < threshold:
-                found_group = False
-                for k, v in same_audio_videos_dict.items():
-                    if i in v or j in v:
-                        v.add(i)
-                        v.add(j)
-                        found_group = True
-                        break
-
-                if not found_group:
-                    same_audio_videos_dict[len(same_audio_videos_dict)] = {i, j}
-
-    same_audio_videos_list = list(same_audio_videos_dict.values())
-
-    print("Same audio video groups:", same_audio_videos_list)
-    return same_audio_videos_list
-
-
-def move_videos_to_folders(same_audio_videos_list):
+def move_videos_to_folders(groups):
     output_folder = 'grouped_videos'
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    for group_index, video_group in enumerate(same_audio_videos_list):
-        group_folder = os.path.join(output_folder, f'group_{group_index + 1}')
+    for i, group in enumerate(groups):
+        group_folder = os.path.join(output_folder, f'group_{i + 1}')
         if not os.path.exists(group_folder):
             os.makedirs(group_folder)
 
-        for video_index in video_group:
-            src = video_files[video_index]
-            dst = os.path.join(group_folder, os.path.basename(src))
-            shutil.copyfile(src, dst)
+        for video_file in group:
+            dst = os.path.join(group_folder, os.path.basename(video_file))
+            shutil.copyfile(video_file, dst)
 
-    print(f"Successfully moved {len(same_audio_videos_list)} groups of similar videos into '{output_folder}' folder.")
-
+    print(f"Successfully moved {len(groups)} groups of similar videos into '{output_folder}' folder.")
 
 folder_path = "videos"
 video_files = [
@@ -116,5 +124,5 @@ video_files = [
 ]
 
 if __name__ == "__main__":
-    main_video_files = main(video_files)
-    move_videos_to_folders(main_video_files)
+    groups = main(video_files)
+    move_videos_to_folders(groups)
